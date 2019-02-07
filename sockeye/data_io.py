07e9ba40f305c6148e83868f0e21ffa7d4bd310e
Annette Rios <arios@rattle.ifi.uzh.ch>
2019-02-07 17:11:03 +0100

    removed unnecessary function

--- sockeye/data_io.py
+++ sockeye/data_io.py
@@ -1099,21 +1099,6 @@ def ids2tokens(token_ids: Iterable[int],
     tokens = (vocab_inv[token] for token in token_ids)
     return (tok for token_id, tok in zip(token_ids, tokens) if token_id not in exclude_set)
 
-def ids2tokens_spm(token_ids: Iterable[int],
-               vocab_inv: Dict[int, str],
-               exclude_set: Set[int]) -> Iterator[str]:
-    """
-    Transforms a list of token IDs into a list of words, excluding any IDs in `exclude_set`.
-
-    :param token_ids: The list of token IDs.
-    :param vocab_inv: The inverse vocabulary.
-    :param exclude_set: The list of token IDs to exclude.
-    :return: The list of words.
-    """
-    tokens = (vocab_inv[token] for token in token_ids)
-    return [tok for token_id, tok in zip(token_ids, tokens) if token_id not in exclude_set]
-
-
 
 class SequenceReader(Iterable):
     """
